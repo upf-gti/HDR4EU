3120235d2173c05856dde1e0f2cd1f8d1165b98c
Maria Bosser <mbcarenys@gmail.com>
2018-12-05 18:53:30 +0100

    ExpTM implementado

--- js/maria.js
+++ js/maria.js
@@ -32,3 +32,30 @@ LogTM.Code = `
 `;
 
 CORE.registerTonemapper( LogTM );
+
+
+/**
+ * xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
+ */
+
+function ExpTM()
+{
+    if(this.constructor != ExpTM)
+        throw("Use new");
+
+    this.uniforms = {};
+}
+
+ExpTM.Uniforms = `
+    uniform float u_logMean;
+`;
+
+ExpTM.Code = `
+
+    float lum = 0.2126 * color.r + 0.7152 * color.g + 0.0722 * color.b;
+    float lum_TM = 1.0 - exp( -0.35 * lum/u_logMean );
+
+    color = color.rgb * lum_TM/lum;
+`;
+
+CORE.registerTonemapper( ExpTM );
